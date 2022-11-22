0df6cc57f29035a9acbfaafe1e7b5bb9436999ad
BIL-LAB5PC05\Ogrenci <123@123.COM>
2022-11-22 11:30:57 +0300

    22,10,2022

---
+++ U2_Uyg22/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U2_Uyg22
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 6; i <= 16; i++)
+            {
+                comboBox1.Items.Add(i);
+            }
+        }
+    }
+}
