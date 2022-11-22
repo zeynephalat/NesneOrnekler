5199b3d9e9606acae6a515f354f7845b64d647e9
BIL-LAB5PC05\Ogrenci <123@123.COM>
2022-11-22 16:43:41 +0300

    no message

---
+++ U2_Uyg27/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U2_Uyg27
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                int sayi = Convert.ToInt32(listBox1.Items[i]);
+                if (sayi % 2 == 0)
+                {
+                    listBox2.Items.Add(listBox1.Items[i]);
+                }
+                else
+                {
+                    listBox3.Items.Add(listBox1.Items[i]);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                listBox1.Items.Add(random.Next(1, 1000));
+            }
+        }
+
+    }
+}
