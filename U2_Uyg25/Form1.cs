29a40e3750336748272b5c0df9e1363d995ff2af
BIL-LAB5PC05\Ogrenci <123@123.COM>
2022-11-22 13:47:50 +0300

    no message

---
+++ U2_Uyg25/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U2_Uyg25
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double kuvvet;
+            for (int i = 0; i < 5; i++)
+            {
+                kuvvet = Math.Pow(10, i);
+                listBox1.Items.Add(kuvvet);
+            }
+        }
+    }
+}
