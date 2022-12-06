102158a405cbc1a7cce180c881f185841c9ce655
BIL-LAB5PC05\Ogrenci <123@123.COM>
2022-12-06 16:14:05 +0300

    no message

---
+++ U2_SS69_2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U2_SS69_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int topla = 0;
+            for (int say = 0; say < 100; say++)
+            {
+                topla = topla + say;
+                MessageBox.Show(topla.ToString());
+                if (topla>=200)
+                {
+                    MessageBox.Show("limit aşıldı");
+                    break;
+                }
+
+            }
+        }
+    }
+}
