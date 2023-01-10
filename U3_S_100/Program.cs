9885d489e03a1b7d87f2ca14902d842ebebc5a44
BIL-LAB5PC05\Ogrenci <123@123.COM>
2023-01-10 13:20:13 +0300

    no message

---
+++ U3_S_100/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_S_100
+{
+    class OkulPersoneli
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+       }
+       public class Ogretmen:OkulPersoneli
+    { 
+        public string Brans { get; set; }
+}
+    class Program
+    { 
+        static void Main(string[]args)
+        { 
