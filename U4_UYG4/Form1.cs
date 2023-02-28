9619f172a181216744adc3ac40ffb9bfc847831b
BIL-LAB1PC08\Ogrenci <123@123.COM>
2023-02-28 12:19:20 +0300

    no message

---
+++ U4_UYG4/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U4_UYG4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] isimler = new string[5];
+        int[] notlar = new int[5];
+        int index = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (index < isimler.Length)
+            {
+                isimler[index] = textBox1.Text;
+                notlar[index] = int.Parse(textBox2.Text);
+                index++;
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (index < isimler.Length)
+            {
+                isimler[index] = textBox1.Text;
+                notlar[index] = int.Parse(textBox2.Text);
+                index++;
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            {
+                if (index < isimler.Length)
+                {
+                    isimler[index] = textBox1.Text;
+                    notlar[index] = int.Parse(textBox2.Text);
+                    index++;
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int enyuksek = notlar[0];
+            for (int i = 0; i < notlar.Length; i++)
+            {
+                if (notlar[i]>enyuksek)
+                {
+                    enyuksek = notlar[i];
+                }
+            }
+            textBox2.Text = textBox2.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int toplam = 0;
+            double ortalama = 0;
+            for (int i = 0; i < notlar.Length; i++)
+            {
+                toplam += notlar[i];
+            }
+            ortalama = toplam / notlar.Length;
+            textBox5.Text = ortalama.ToString();
+        }
+    }
+}
---
+++ U4_uyg6/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U4_uyg6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] isimler = new string[3];
+       int[,] notlar = new int[3,4];
+
+        int index = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isimler[index] = textBox1.Text;
+            notlar[index, 0] = int.Parse(textBox2.Text);
+            notlar[index, 1] = int.Parse(textBox3.Text);
+            notlar[index, 2] = int.Parse(textBox4.Text);
+            notlar[index, 3] = int.Parse(textBox5.Text);
+            index++;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double toplam;
+            for (int x = 0; x < 3; x++)
+            {
+                toplam = 0;
+                for (int y = 0; y < 4; y++)
+                {
+                    toplam += notlar[x, y];
+                }
+                listBox1.Items.Add(isimler[x] + "=>" + toplam / 4);
+            }
+        }
+    }
+}
