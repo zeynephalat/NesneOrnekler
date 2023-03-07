144199b10a66ea00a6deb779e7a7b13a2b949b31
BIL-LAB1PC08\Ogrenci <123@123.COM>
2023-03-07 14:24:55 +0300

    no message

---
+++ U5_Uyg1/FormA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U5_Uyg1
+{
+    public partial class FormA : Form
+    {
+        public FormA()
+        {
+            InitializeComponent();
+        }
+    }
+}
